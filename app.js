6ea1549de6f56a6105b495b1c9d5aa54fa2ae266
abhrajo <ajduttaray@gmail.com>
2021-11-29 05:08:01 +0530

    init

---
+++ app.js
@@ -0,0 +1,51 @@
+const nodesList = document.getElementById('nodesList');
+const searchBar = document.getElementById('searchBar');
+
+let nodeIds = [];
+
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredNodes = nodeIds.filter((nodes) => {
+        return (
+            nodes.provider_id.toLowerCase().includes(searchString)
+        );
+    });
+    displayNodes(filteredNodes);
+});
+
+const loadNodes = async () => {
+    try {
+        const res = await fetch('https://protected-island-29616.herokuapp.com/https://discovery.mysterium.network/api/v3/proposals', {
+		headers: {
+			'Cache-Control': 'max-age=0, no-cache, no-store, must-revalidate'
+				}
+					});
+		
+        nodeIds = await res.json();
+        displayNodes(nodeIds);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayNodes = (nodes) => {
+    const htmlString = nodes
+        .map((nodes) => {
+            return `
+            <li class="nodes">
+                <p><b>${nodes.provider_id}</b></p>
+				<p><b>IP Type:</b> ${nodes.location.ip_type}</p>
+				<p><b>Quality:</b> ${nodes.quality.quality}</p>
+				<p><b>Latency:</b> ${nodes.quality.latency}</p>
+				<p><b>Bandwidth:</b> ${nodes.quality.bandwidth}</p>
+                
+            </li>
+        `;
+        })
+        .join('');
+    nodesList.innerHTML = htmlString;
+};
+
+loadNodes();
